2f328171f5c48717f9633cbf528b84e2fc55869a
Rajahni fields <rajahnifields@gmail.com>
2020-12-16 13:32:18 -0500

    figure=ing out why the forecast is not posting

--- weather.js
+++ weather.js
@@ -1,22 +1,82 @@
-// $.ajax({
-//     url: queryURL,
-//     method: "GET"
-//   }).then(function(response) {
+  const APIKey ="ac42371c43ac990da2890a1cf869a51b"
+  var cityN= $("#searchB").val()
+  var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
+  "q=+" + cityN +"&appid=" + APIKey;
 
-//     // Create CODE HERE to Log the queryURL
-//     console.log (APIKey)
-//     console.log(response)
-//     // Create CODE HERE to log the resulting object
-//     // Create CODE HERE to calculate the temperature (converted from Kelvin)
-//     console.log((response.main.temp)-273);
-//     // Create CODE HERE to transfer content to HTML
-//     // Hint: To convert from Kelvin to Fahrenheit: F = (K - 273.15) * 1.80 + 32
-//     // Create CODE HERE to dump the temperature content into HTML
-//     var queryURL = "pro.openweathermap.org/data/2.5/forecast/hourly?q={city name}&appid={ac42371c43ac990da2890a1cf869a51b}";
-//     console.log (queryURL)
-//   });
-  //search work
-  let searchbtn =$(".searchbtn")
-  $(".searchbtn").on('click',function(){
-      alert("yay it works")
-  })
+    let searchbtn =$(".searchbtn")
+  $(".searchbtn").on('click',function(event){
+      event.preventDefault();
+    alert("yay it works")
+
+    var cityN= $("#searchB").val()
+    console.log($("#searchB"))
+
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
+  "q=+" + cityN +"&appid=" + APIKey;
+
+    $(".weatherCard").addClass("show")
+
+    $.ajax({
+      
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+      // Display response in the console log
+      console.log(response)
+    });
+
+
+ // calculate the response
+   let tempD =(response.main.temp - 273.15)* 1.80 + 32
+   console.log (Math.floor(tempD))
+   console.log(response.wind.speed)
+   getCurrentConditions(response)
+   getCurrentForecast(response)
+   listcity() 
+ 
+      })
+   function listcity() {
+
+    let listR =$("<li>").addClass("list-group-item").text(cityN)
+    $(".list").append(listR)
+   }
+
+   function getCurrentConditions(response) {
+    let tempD =(response.main.temp - 273.15)* 1.80 + 32
+    tempD= math.floor(tempD)
+
+    $(".weatherCard").empty()
+
+    const card = $("<div>").addClass("card")
+    const cardB = $("<div>").addClass("card-body")
+    const sCity = $("<h4>").addClass("card-title").text(response.name)
+    const cityD = $("<h4>").addClass("card-title").text(date.tolocaleDateString('en-US'))
+    const Temperture = $("<p>").addClass("card-text current-temp").text("temperature:"+tempD+ "°F")
+    const humidity = $("<p>").addClass("card-text current-humidity").text("humidity:"+response.main.humidity+ "%")
+    const wind = $("<p>").addClass("card-text current-wind").text("Wind Speed:"+response.wind.speed+ "MPH")
+    const image = $("<img>").attr("src","https://openweathermap.org/img/w/"+ response.weather[0].icon+"png")
+
+    //add to page
+    cityN.append(cityD, image)
+    cardB.append(sCity, Temperture, humidity, wind)
+    card.append(cardB)
+    $(".weatherCard").append(card)
+
+   }
+   $.ajax({
+ 
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    // Display response in the console log
+    console.log(response)
+    $("")
+
+
+  });
+
+    
+
+  
+
+  
