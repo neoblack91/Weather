f5cd3189f69447f518a3bf1b5791c18b80c54133
Rajahni fields <rajahnifields@gmail.com>
2020-12-17 23:03:47 -0500

    git commit .

--- weather.js
+++ weather.js
@@ -1,6 +1,9 @@
   const apiKey = "ac42371c43ac990da2890a1cf869a51b"
-  var cityN= $("#searchB").val()
-  var QueryURL = "https://api.openweathermap.org/data/2.5/weather?"+"q="+cityN+"&appid="+ apiKey;
+
+  // let Date =  newDate()
+
+ 
+  
 
     let searchbtn =$(".searchbtn")
     
@@ -8,13 +11,13 @@
       event.preventDefault();
     // alert("yay it works")
     $("#WForecastH5").addClass("show")
-
+    var cityN= $("#searchB").val()
   
     var QueryURL = "https://api.openweathermap.org/data/2.5/weather?"+"q="+cityN+"&appid="+ apiKey;
+    console.log(QueryURL)
     $.ajax({
-      
       url: QueryURL,
-      method: "GET"
+      method: "GET",
     }).then(function(response) {
       // Display response in the console log
       console.log(response)
@@ -24,27 +27,28 @@
         getCurrentConditions(response)
         getCurrentForecast(response)
         listcity() 
-
+      console.log(cityN)
+      
     });
 
  })
  // calculate the response
-   function listcity() {
-
+    function listcity() {
+    console.log ("hello from listcity")
     let listR =$("<li>").addClass("list-group-item").text(cityN)
     $(".list").append(listR)
    }
 
    function getCurrentConditions(response) {
     let tempD =(response.main.temp - 273.15)* 1.80 + 32
-    tempD= math.floor(tempD)
-
+    tempD= Math.floor(tempD)
+    console.log ("hello from getcurrentconditions")
     $(".weatherCard").empty()
 
     const card = $("<div>").addClass("card")
     const cardB = $("<div>").addClass("card-body")
     const sCity = $("<h4>").addClass("card-title").text(response.name)
-    const cityD = $("<h4>").addClass("card-title").text(date.tolocaleDateString('en-US'))
+    const cityD = $("<h4>").addClass("card-title").text(Date.tolocaleDateString('en-US'))
     const Temperture = $("<p>").addClass("card-text current-temp").text("temperature:"+tempD+ "°F")
     const humidity = $("<p>").addClass("card-text current-humidity").text("humidity:"+response.main.humidity+ "%")
     const wind = $("<p>").addClass("card-text current-wind").text("Wind Speed:"+response.wind.speed+ "MPH")
@@ -59,23 +63,27 @@
    }
    function getCurrentForecast (){
       $.ajax({
-      
-          url: queryURL,
-          method: "GET"
+          url: QueryURL,
+          method: "GET",
         }).then(function(response) {
-          // Display response in the console log
-          
-          $("#WForecast").empty()
+          console.log (response)
+           $("#WForecast").empty()
 
            let results = response.listR
           console.log(results)
 
           for (let r =0; r< results.length; r++){
+
+            let day = number(results[r].dt_txt.split('-')[2].split(' ')[0])
+            let hour = results[r].dt_txt.split('-')[2].split(' ')[1]
+
+            if(results[r].dt_txt.index0f("12:00")!== -1){
+
             let temp =(results[r].main.temp- 273.15) * 1.80 +32
             let tempD =Math.floor(temp)
-             const card = $("<div>").addClass("card col-md-2 ml-4 bg-primary text-white");
+             const card = $("<div>").addClass("card-body");
              const cardB = $("<div>").addClass("card-body p-3 WForecastBody")
-             const cityD = $("<h4>").addClass("card-title").text(date.toLocaleDateString('en-US'));
+             const cityD = $("<h4>").addClass("card-title").text(Date.toLocaleDateString('en-US'));
              const Temperature = $("<p>").addClass("card-text forecastTemp").text("Temperature: " + tempF + " °F");
              const humidity = $("<p>").addClass("card-text forecastHumidity").text("Humidity: " + results[i].main.humidity + "%");
 
@@ -85,7 +93,7 @@
             card.append(cardB);
             $("#WForecast").append(card);
 
-
+            }
           }
 
         });
